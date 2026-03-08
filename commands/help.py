23eb409fc5310492c97525613b2c0e2041a7806a
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 16:43:28 +0530

    Add files via upload

---
+++ commands/help.py
@@ -0,0 +1,25 @@
+def register(bot):
+    @bot.message_handler(commands=['help'])
+    def help_cmd(message):
+        bot.reply_to(message,
+"""
+🎮 Game Commands:
+!status
+!attack
+!kill
+!rob
+!give
+!deposit / !withdraw
+!claim
+
+🛒 Shop:
+!shop
+/use bandage
+/use medkit
+
+🛡 Protection:
+!shield
+
+Admin Commands:
+!block
+""")

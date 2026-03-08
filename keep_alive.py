f2c8d99ad8d7c3d6bdff763a3cf42ee39cd863a3
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 17:21:37 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask("")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -12,4 +12,5 @@ def run():
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()

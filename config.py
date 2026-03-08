ecd4780f921e9ff8eb73fce7a8835e9b1e766384
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 17:46:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "8612687420:AAGbnCt_M5zCy-HygizKLjNwaA6lJz8Zr6k"
 OWNER_ID = 8721663490
 ADMINS = [6011975278, 2145828547, 7501472798, 7299000709]
-MONGO_URI = "mongodb+srv://gojosaturo143k_db_user:hacker14rk@cluster0.sajt3dn.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://chotabheem14rk14_db_user:Hacker14rk@cluster0.xkhzdvx.mongodb.net/?appName=Cluster0"

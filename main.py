5bb60fd1c78786fc4805d9a5f0d364b205f64234
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 22:32:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from keep_alive import keep_alive
 from commands import start, help, status, attack, kill, rob, give, bank, claim, shield, block
 from systems import shop, inventory, xp_rank
 from commands import leaderboard
+from commands import addmoney
+
+
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -22,6 +25,7 @@ claim.register(bot)
 shield.register(bot)
 block.register(bot)
 leaderboard.register(bot)
+addmoney.register(bot)
 
 shop.register(bot)
 inventory.register(bot)

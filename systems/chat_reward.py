3a22ad01a374fe6ddd90d1a5b2ec824cfdb38954
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 22:22:35 +0530

    Update chat_reward.py

--- systems/chat_reward.py
+++ systems/chat_reward.py
@@ -1,21 +1,24 @@
 from database import users
 from systems.anti_spam import is_spam
-from systems.xp_rank import add_xp
-
-CHAT_LIMIT = 500
-REWARD = 1000
+import time
 
 def process_chat(user_id, name):
 
     # spam check
-    if is_spam(user_id):
-        return
+    spam_time = is_spam(user_id)
 
-    user = users.find_one({"_id": user_id})
+    if spam_time:
+        users.update_one(
+            {"id": user_id},
+            {"$set": {"blocked_until": spam_time}}
+        )
+        return "🚫 Spam detected! You are blocked for 6 hours."
+
+    user = users.find_one({"id": user_id})
 
     if not user:
         users.insert_one({
-            "_id": user_id,
+            "id": user_id,
             "name": name,
             "health": 100,
             "money": 0,
@@ -23,39 +26,27 @@ def process_chat(user_id, name):
             "xp": 0,
             "rank": "Bronze",
             "guns": [],
-            "medico": {},
+            "bandage": 0,
+            "medkit": 0,
             "shield": 0,
-            "chat_count": 0
+            "chat": 0,
+            "blocked_until": 0
         })
-        user = users.find_one({"_id": user_id})
+        user = users.find_one({"id": user_id})
 
-    chat_count = user.get("chat_count", 0) + 1
+    # block check
+    if user.get("blocked_until", 0) > time.time():
+        return None
 
-    # reward system
-    if chat_count >= CHAT_LIMIT:
-        users.update_one(
-            {"_id": user_id},
-            {
-                "$inc": {
-                    "money": REWARD
-                },
-                "$set": {
-                    "chat_count": 0
-                }
+    # 1 coin per message
+    users.update_one(
+        {"id": user_id},
+        {
+            "$inc": {
+                "money": 1,
+                "chat": 1
             }
-        )
-
-        add_xp(user_id, 10)
-        return f"🎁 {name} ko 500 chats complete hone par **{REWARD} coins** mile!"
-
-    else:
-        users.update_one(
-            {"_id": user_id},
-            {
-                "$set": {
-                    "chat_count": chat_count
-                }
-            }
-        )
+        }
+    )
 
     return None

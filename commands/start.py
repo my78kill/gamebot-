9838dcbb7f89f41176c0bf59bb67b004c51da8a8
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 22:18:54 +0530

    Update start.py

--- commands/start.py
+++ commands/start.py
@@ -1,13 +1,20 @@
 from database import users
+import time
 
 def register(bot):
+
     @bot.message_handler(commands=['start'])
-    def start(message):
-        user = users.find_one({"id": message.from_user.id})
+    def start_cmd(message):
+
+        user_id = message.from_user.id
+        name = message.from_user.first_name
+
+        user = users.find_one({"id": user_id})
+
         if not user:
             users.insert_one({
-                "id": message.from_user.id,
-                "name": message.from_user.first_name,
+                "id": user_id,
+                "name": name,
                 "money": 1000,
                 "bank": 0,
                 "health": 100,
@@ -17,15 +24,25 @@ def register(bot):
                 "bandage": 0,
                 "medkit": 0,
                 "shield": 0,
+                "dead_until": 0,
                 "chat": 0
             })
+
         bot.reply_to(message, f"""
-🎮 Welcome {message.from_user.first_name} to Battle Game Bot!
+🎮 **Welcome to Battle Arena**
 
-⚔ Attack players, kill, rob
-🏦 Bank system, claim daily
-🛒 Shop & buy guns/medico/shields
-💬 Chat reward system
+Hello **{name}** 👋
 
-Type !help to see all commands
+🔥 **Game Features**
+• Attack other players
+• Rob money from enemies
+• Kill players with powerful guns
+• Buy weapons & healing items
+• Rank system with XP
+• Global leaderboard
+• Bank system
+
+⚔ **Start your journey now!**
+
+Type **/help** to see all commands.
 """)

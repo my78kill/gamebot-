a978a44fe69dc1cc684171ee796089dff31120bf
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 22:42:00 +0530

    Update xp_rank.py

--- systems/xp_rank.py
+++ systems/xp_rank.py
@@ -10,12 +10,30 @@ RANKS = [
 ]
 
 def add_xp(user_id, amount):
+
     user = users.find_one({"id": user_id})
-    new_xp = user.get("xp", 0) + amount
-    new_rank = user.get("rank","Bronze")
+
+    if not user:
+        return None
+
+    current_xp = user.get("xp", 0)
+    current_rank = user.get("rank", "Bronze")
+
+    new_xp = current_xp + amount
+    new_rank = current_rank
+
     for rank_name, xp_required in reversed(RANKS):
         if new_xp >= xp_required:
             new_rank = rank_name
             break
-    users.update_one({"id": user_id},{"$set":{"xp": new_xp, "rank": new_rank}})
-    return new_xp, new_rank
+
+    users.update_one(
+        {"id": user_id},
+        {"$set": {"xp": new_xp, "rank": new_rank}}
+    )
+
+    # check if rank changed
+    if new_rank != current_rank:
+        return new_xp, new_rank, True
+
+    return new_xp, new_rank, False

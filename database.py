3ad5ff6be3566fa29182a06a373cd4281d7a0502
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 18:12:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,13 @@
 from pymongo import MongoClient
 from config import MONGO_URI
+import certifi
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+
 db = client["battle_game"]
 
 users = db["users"]

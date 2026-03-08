cb8791769aaaebc80c4b4adbc655a468fcdcb2cf
my78kill <aigirlfriend143you@gmail.com>
2026-03-08 16:39:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import telebot
+from config import BOT_TOKEN
+from keep_alive import keep_alive
+
+from commands import start, help, status, attack, kill, rob, give, bank, claim, shield, block
+from systems import shop, inventory, xp_rank, chat_reward, anti_spam
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# Register commands
+start.register(bot)
+help.register(bot)
+status.register(bot)
+attack.register(bot)
+kill.register(bot)
+rob.register(bot)
+give.register(bot)
+bank.register(bot)
+claim.register(bot)
+shield.register(bot)
+block.register(bot)
+
+shop.register(bot)
+inventory.register(bot)
+anti_spam.register(bot)
+
+keep_alive()
+bot.infinity_polling()
